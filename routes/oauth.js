996fd7d914a1aca69f5ce5d22f6513b1532b0b36
Rorum <phoo8651@gmail.com>
2023-07-31 13:30:09 +0900

    update

--- routes/oauth.js
+++ routes/oauth.js
@@ -7,6 +7,7 @@ const { Twitch, Minecraft } = require("../mongo");
 require("dotenv").config();
 
 let TwitchRedirectUri = process.env.Url + "/oauth/twitch/callback";
+let OptionsRedirectUri = process.env.Url + "/oauth/options/callback";
 let XboxLiveRedirectUri = process.env.Url + "/oauth/xboxlive/callback";
 
 let isFollowed = (followedChannels, broadcasterId) => {
@@ -17,6 +18,103 @@ let isFollowed = (followedChannels, broadcasterId) => {
   }
   return false;
 };
+
+// 스트리머 본인이나 관리자 또는 트위치 시스템 오류로 인해 트위치 연동이 안되는 사람들은
+// https://내도메인/oauth/options/login 으로 접속하여 트위치 연동을 시도할 수 있습니다.
+router.get("/options/login", (req, res) => {
+  let id = v4();
+  let state = id.replace(/-/g, "");
+  const params = qs.stringify({
+    client_id: process.env.TwitchClientId,
+    redirect_uri: OptionsRedirectUri,
+    response_type: "code",
+    scope: process.env.TwitchScopes,
+    state: state,
+  });
+  res.redirect(`${process.env.TwitchRequestUri}${params}`);
+});
+
+router.get("/options/callback", async (req, res) => {
+  const code = req.query.code;
+  const state = req.query.state;
+  try {
+    const tokenParams = qs.stringify({
+      client_id: process.env.TwitchClientId,
+      client_secret: process.env.TwitchClientSecret,
+      code: code,
+      grant_type: "authorization_code",
+      redirect_uri: TwitchRedirectUri,
+    });
+
+    const response = await axios.post(
+      process.env.TwitchResponseUri,
+      tokenParams,
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+
+    const accessToken = response.data.access_token;
+    const TwitchApiEndpoint = `${process.env.TwitchAPIUri}/users`;
+    const userResponse = await axios.get(TwitchApiEndpoint, {
+      headers: {
+        "Client-ID": process.env.TwitchClientId,
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    const user = userResponse.data.data[0];
+    const userId = user.id;
+    const userDisplay = user.display_name;
+    const userEmail = user.email;
+
+    const twitch = new Twitch({
+      _id: state,
+      TwitchId: userId,
+      TwitchName: userDisplay,
+      TwitchEmail: userEmail,
+    });
+
+    twitch
+      .save()
+      .then((result) => {
+        console.log(result);
+        res.redirect(
+          `${process.env.Url}/oauth/xboxlive/login?state=${state}`
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+        res.render("error-handler", {
+          title: "Database Error",
+          error: "500",
+          message: "DB에 데이터를 저장하지 못했습니다.",
+          Title: process.env.Title,
+          Company: process.env.Company,
+          Url: process.env.Url,
+          TwitchName: process.env.TwitchName,
+          TwitchLink: process.env.TwitchLink,
+          YoutubeLink: process.env.YoutubeLink,
+          Email: process.env.Email,
+        });
+      });
+  } catch (error) {
+    res.render("error-handler", {
+      title: "Twitch Error",
+      error: "403",
+      message: "트위치 계정을 가져오지 못했습니다.",
+      Title: process.env.Title,
+      Company: process.env.Company,
+      Url: process.env.Url,
+      TwitchName: process.env.TwitchName,
+      TwitchLink: process.env.TwitchLink,
+      YoutubeLink: process.env.YoutubeLink,
+      Email: process.env.Email,
+    });
+  }
+});
 
 router.get("/twitch/login", (req, res) => {
   let id = v4();
